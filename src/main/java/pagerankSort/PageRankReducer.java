b5a42ba9fc91f1c07a18536d39b5d7f4a05f84bb
maver <maverickers@outlook.com>
2018-11-12 17:04:21 -0800

    add pagerank sort

---
+++ src/main/java/pagerankSort/PageRankReducer.java
@@ -0,0 +1,41 @@
+package pagerankSort;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class PageRankReducer extends Reducer<Text, Text, Text, Text> {
+	private static final Log log = LogFactory.getLog(PageRankReducer.class);
+
+    private static float factor = 0.85f;
+
+    @Override
+    protected void setup(Context context) throws IOException,
+            InterruptedException {
+        // factor default value 0.85f
+        factor = context.getConfiguration().getFloat("mapred.pagerank.factor",
+                0.85f);
+    }
+
+	@Override
+	protected void reduce(Text key, Iterable<Text> values, Context context)
+			throws IOException, InterruptedException {
+		log.info(key.toString());
+		float rank = 1 - factor;// PageRankֵ
+		String[] str;
+		Text outLinks = new Text();
+		for (Text t : values) {
+			str = t.toString().split(";");
+			if (str.length == 3) {
+				rank += Float.parseFloat(str[1]) / Integer.parseInt(str[2])
+						* factor; // page rank formula
+			} else {
+				outLinks.set(t.toString());
+			}
+		}
+		context.write(new Text(key.toString() + "," + rank), outLinks);
+	}
+}
---
+++ src/main/java/pagerankSort/PageText.java
@@ -0,0 +1,82 @@
+package pagerankSort;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class PageText implements WritableComparable<PageText> {
+
+    public Text page; // page Text
+    public FloatWritable rank;// get PR
+    private IntWritable count;// outlink length
+
+    public Text getPage() {
+        return page;
+    }
+
+    public FloatWritable getRank() {
+        return rank;
+    }
+
+    public IntWritable getCount() {
+        return count;
+    }
+
+    public PageText() {
+        this(new Text(), new FloatWritable(), new IntWritable());
+    }
+
+    public PageText(String page, float rank, int count) {
+        this(new Text(page), new FloatWritable(rank), new IntWritable(count));
+    }
+
+    public PageText(Text page, FloatWritable rank, IntWritable count) {
+        super();
+        this.page = page;
+        this.rank = rank;
+        this.count = count;
+    }
+
+    public PageText(Text page) {
+        super();
+        this.page = page;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        page.readFields(in);
+        rank.readFields(in);
+        count.readFields(in);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        page.write(out);
+        rank.write(out);
+        count.write(out);
+    }
+
+    @Override
+    public int compareTo(PageText arg0) {
+        // TODO Auto-generated method stub
+        PageText other = (PageText) arg0;
+        if (this.getRank().get() > other.getRank().get())
+            return -1;
+        if (this.getRank().get() < other.getRank().get())
+            return 1;
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+//        return super.toString();
+//        return this.getRank().get()+";"+this.getCount().get();
+        return this.getPage()+","+this.getRank();
+    }
+}
+
